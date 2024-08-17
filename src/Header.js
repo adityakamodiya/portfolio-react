f982a86854be1b7d046d5cec69f2e726e7475299
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-18 00:05:24 +0530

    commit

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,49 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import './CSS/Header.css'; // Make sure to import your CSS file
+
 function Header() {
+  const [waveActive, setWaveActive] = useState(false);
+
+  const handleClick = (e) => {
+    setWaveActive(true);
+
+    // Reset the wave effect after animation ends
+    setTimeout(() => {
+      setWaveActive(false);
+    }, 1000); // Match the duration of the animation
+  };
+
   return (
     <>
-        <nav>
-            <ul>
-                <li><Link to='/' ><i class="fa-solid fa-house"></i></Link></li>
-                <li><Link to='about' ><i class="fa-solid fa-user"></i></Link></li>
-                <li><Link to='work' ><i class="fa-solid fa-briefcase"></i></Link></li>
-                <li><Link to='qualification' ><i class="fa-solid fa-graduation-cap"></i></Link></li>
-                {/* <li><Link to='portfolio' ><i class="fa-solid fa-holly-berry"></i></Link></li> */}
-                <li><Link to='contact' ><i class="fa-solid fa-address-book"></i></Link></li>
-                <li><Link to='skills' ><i class="fa-solid fa-layer-group"></i></Link></li>
-                
-                {/* <li><Link to='about' ><i class="fa-solid fa-user"></i></Link></li> */}
-                {/* <li><Link to={''} ></Link></li> */}
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li className="nav-link" onClick={handleClick}>
+            <Link to='/'><i className="fa-solid fa-house"></i></Link>
+          </li>
+          <li className="nav-link" onClick={handleClick}>
+            <Link to='about'><i className="fa-solid fa-user"></i></Link>
+          </li>
+          <li className="nav-link" onClick={handleClick}>
+            <Link to='work'><i className="fa-solid fa-briefcase"></i></Link>
+          </li>
+          <li className="nav-link" onClick={handleClick}>
+            <Link to='qualification'><i className="fa-solid fa-graduation-cap"></i></Link>
+          </li>
+          <li className="nav-link" onClick={handleClick}>
+            <Link to='contact'><i className="fa-solid fa-address-book"></i></Link>
+          </li>
+          <li className="nav-link" onClick={handleClick}>
+            <Link to='skills'><i className="fa-solid fa-layer-group"></i></Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="wave-effect-container">
+        {waveActive && <div className="bright-out"></div>}
+        {waveActive && <div className={`wave-effect`}></div>}
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
