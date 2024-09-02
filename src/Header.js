aaa0ecb0e3b7c1a1cc570e3b1bfd78631c46d471
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-09-02 14:43:51 +0530

    hover effect added

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import './CSS/Header.css'; // Make sure to import your CSS file
+import './CSS/Header.css'; // Import your CSS file
 import './portfoluo.css'
 
 function Header() {
@@ -21,21 +21,27 @@ function Header() {
         <ul>
           <li className="nav-link" onClick={handleClick}>
             <Link to='/'><i className="fa-solid fa-house"></i></Link>
+            <span className="tooltip">Home</span>
           </li>
           <li className="nav-link" onClick={handleClick}>
             <Link to='about'><i className="fa-solid fa-user"></i></Link>
+            <span className="tooltip">About</span>
           </li>
           <li className="nav-link" onClick={handleClick}>
             <Link to='work'><i className="fa-solid fa-briefcase"></i></Link>
+            <span className="tooltip">Work</span>
           </li>
           <li className="nav-link" onClick={handleClick}>
             <Link to='qualification'><i className="fa-solid fa-graduation-cap"></i></Link>
+            <span className="tooltip">Qualification</span>
           </li>
           <li className="nav-link" onClick={handleClick}>
             <Link to='contact'><i className="fa-solid fa-address-book"></i></Link>
+            <span className="tooltip">Contact</span>
           </li>
           <li className="nav-link" onClick={handleClick}>
             <Link to='skills'><i className="fa-solid fa-layer-group"></i></Link>
+            <span className="tooltip">Skills</span>
           </li>
         </ul>
       </nav>
